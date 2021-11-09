8c7e9b19a9d88b81722d65bd933d37b7d8352926
Your Name <veronica-a@live.se>
2021-11-09 15:28:30 +0000

    Add routes for bikes

--- SparkApi/routes/web.php
+++ SparkApi/routes/web.php
@@ -16,3 +16,12 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->group(['prefix' => 'sparkapi/v1'], function () use ($router) {
+
+      $router->get('bikes',  ['uses' => 'BikeController@showAllBikes']);
+      $router->get('bikes/{id}', ['uses' => 'BikeController@showOneBike']);
+      $router->post('bikes', ['uses' => 'BikeController@create']);
+      $router->delete('bikes/{id}', ['uses' => 'BikeController@delete']);
+      $router->put('bikes/{id}', ['uses' => 'BikeController@update']);
+});
