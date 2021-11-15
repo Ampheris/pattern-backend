0b6746bd7c6cd279a74694e7b0d65cbd7fe98900
Rilr20 <nrrl99@gmail.com>
2021-11-15 17:37:01 +0100

    changed table names and made available nullable

--- SparkApi/app/Models/Chargingstation.php
+++ SparkApi/app/Models/Chargingstation.php
@@ -10,8 +10,8 @@ class Chargingstation extends Model
     use HasFactory;
 
     protected $fillable = [
-        'x_pos',
-        'y_pos',
+        'X',
+        'Y',
         'radius',
         'available',
         'name'
--- SparkApi/database/migrations/2021_11_05_151801_create_chargingstations_table.php
+++ SparkApi/database/migrations/2021_11_05_151801_create_chargingstations_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Chargingstation;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,15 +16,23 @@ class CreateChargingstationsTable extends Migration
     {
         Schema::create('chargingstations', function (Blueprint $table) {
             $table->id();
-            $table->float('x_pos');
-            $table->float('y_pos');
+            $table->float('X');
+            $table->float('Y');
             $table->float('radius');
             $table->integer('available');
             $table->string('name');
             $table->timestamps();
         });
+        $this->migrationCreate();
     }
-
+    public function migrationCreate() {
+        $station = new Chargingstation();
+        $station->X = 0.025;
+        $station->Y = 0.025;
+        $station->radius = 0.005;
+        $station->name = 'hamnen';
+        $station->save();
+    }
     /**
      * Reverse the migrations.
      *
