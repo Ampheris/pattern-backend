5bbd2976b2bb22288285fba54a4fee1a305e1463
Mathilda Holmström <mathilda.holmstrom@telia.se>
2021-12-25 18:09:39 +0100

    Start and stop bikeride

--- SparkApi/app/Http/Middleware/OAuthenticate.php
+++ SparkApi/app/Http/Middleware/OAuthenticate.php
@@ -5,7 +5,6 @@ namespace App\Http\Middleware;
 use Carbon\Carbon;
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Contracts\Auth\Factory as Auth;
 use Illuminate\Support\Facades\DB;
 
 class OAuthenticate
@@ -23,18 +22,16 @@ class OAuthenticate
             return $next($request);
         }
 
-        // User has an access token cookie
-        if ($request->hasCookie('access_token')) {
-            $access_token = $request->cookie('access_token');
-        }else if ($request->hasHeader('Authorization')) {
+        // User has an bearer token
+        if ($request->bearerToken()) {
+            $access_token = $request->bearerToken();
+        }
+
+        // If uesr has header authorization.
+        if ($request->hasHeader('Authorization')) {
             $access_token = $request->header('Authorization');
             $token_arr = explode(' ', $access_token);
             $access_token = $token_arr[1];
-        }
-        else {
-            // User does not have an access token cookie, but maybe an api key?
-            // todo: add api key check
-            return response('Unauthorized', 401);
         }
 
         try {
